e4d8a1e047be4abef58a4bf7bb6474f303b1b4aa
kumet <kumetsky2014@gmail.com>
2024-09-05 00:22:58 +0300

    Header done!

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ExchangeRateService } from '../../exchange-rate.service';
+import {NgFor} from "@angular/common";
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  exchangeRates: any[] = [];
 
+  constructor(private exchangeRateService: ExchangeRateService) { }
+
+  ngOnInit(): void {
+    this.exchangeRateService.getExchangeRates().subscribe(
+      (data) => {
+        this.exchangeRates = data.filter((item: { cc: string; }) => item.cc === "USD" || item.cc === "EUR");
+        console.log(this.exchangeRates)
+      },
+      (error) => {
+        console.error('Error fetching exchange rates', error);
+      }
+    );
+  }
 }
